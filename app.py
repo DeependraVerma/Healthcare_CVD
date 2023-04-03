13b208b23bc6b4ac006a3b143103e03cacc13678
Deependra Verma <dependra.verma00@gmail.com>
2023-04-03 18:31:47 +0530

    This is the commit for web application

---
+++ app.py
@@ -0,0 +1,28 @@
+import pickle
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+
+## Load the  model
+LR_model = pickle.load(open("healthcare_LR.pkl","rb"))
+scaler_model = pickle.load(open("scaler.pkl","rb"))
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/predict_api',methods = ['Post'])
+def predict_api():
+    data = request.json["data"]
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data = scaler_model.transform(np.array(list(data.values())).reshape(1,-1))
+    output = LR_model.predict(new_data)
+    print(output[0])
+    return(jsonify(float(output[0])))
+
+if __name__=="__main__":
+    app.run(debug=True)
+
